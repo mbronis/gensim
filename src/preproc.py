db2882fd53ea94023a8fc7d1123e782afa291a73
mbronis <michal.bronis@gmail.com>
2021-10-25 01:23:39 +0200

    add TextCleaner

--- src/preproc.py
+++ src/preproc.py
@@ -1,3 +1,5 @@
+import re
+
 import pandas as pd
 
 from src.transformers import PassThroughMixin
@@ -16,5 +18,45 @@ class RawDataCleaner(PassThroughMixin):
 
         X_clean = X_clean[['desc', 'len', 'negative']]
         X_clean = X_clean.reset_index(drop=True)
+
+        return X_clean
+
+
+class TextCleaner(PassThroughMixin):
+    """Cleans texts of polish language and filters failed datum"""
+
+    def __init__(self, keep_digits: int = 1, keep_punct: int = 0) -> None:
+        self.keep_digits = keep_digits
+        self.keep_punct = keep_punct
+
+    @staticmethod
+    def clean_text(s: str, keep_digits: int = 1, keep_punct: int = 0) -> str:
+        """Removes non letter characters from string, optionally keeps digits and punctuation."""
+        special = '[\n\r]+'
+        
+        pattern_flag = tuple([keep_digits, keep_punct])
+        pattern_fact = {
+            (0, 0): r'[^a-ząćęłńóśżźA-ZĄĆĘŁŃÓŚŻŹ\s]',
+            (0, 1): r'[^a-ząćęłńóśżźA-ZĄĆĘŁŃÓŚŻŹ\s!?\'"\.,\-()]',
+            (1, 0): r'[^0-9a-ząćęłńóśżźA-ZĄĆĘŁŃÓŚŻŹ\s]',
+            (1, 1): r'[^0-9a-ząćęłńóśżźA-ZĄĆĘŁŃÓŚŻŹ\s!?\'"\.,\-()]',
+        }
+        pattern = pattern_fact[pattern_flag]
+        
+        s = re.sub(special, ' ', s)
+        s = re.sub(r'\s+', ' ', s)
+        s = re.sub(pattern, '', s)
+        s = s.strip()
+        
+        return s
+
+    def transform(self, X: pd.DataFrame, y: pd.Series = None) -> pd.DataFrame:
+        X_clean = X.copy()
+
+        cleaner = lambda x: TextCleaner.clean_text(x, keep_digits=self.keep_digits, keep_punct=self.keep_punct)
+        X_clean['desc_clean'] = X_clean['desc'].apply(cleaner)
+        X_clean['len_clean'] = X_clean['desc_clean'].apply(len)
+        X_clean['len_clean_ratio'] = X_clean['len_clean'] / X_clean['len']
+        X_clean = X_clean[X_clean['len_clean'] > 1]
 
         return X_clean
