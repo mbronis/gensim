d573c5f953b476e9b3e12b68db16264a2f296b19
mbronis <michal.bronis@gmail.com>
2021-10-26 22:54:32 +0200

    separate load and preprocessing in prep_data.py

--- prep_data.py
+++ prep_data.py
@@ -11,7 +11,7 @@ from src.git_parser import GitParser
 from src.utils import Logger, prep_df_metadata
 from src.exceptions import GitIsDirtyException
 from src.pipelines import data_preproc_pipe
-from src.io import DataPickler
+from src.io import CsvDataLoader, DataPickler
 
 
 if __name__ == '__main__':
@@ -23,18 +23,24 @@ if __name__ == '__main__':
     if git_parser.is_dirty():
         raise GitIsDirtyException()
 
+    logger.log('load raw data')
+    loader = CsvDataLoader(file_name='polish_sentiment_dataset.csv')
+    df_raw = loader.transform(None)
+    logger.log(f'loaded {len(df_raw)} rows')
+
     logger.log('processing data')
     pipe = data_preproc_pipe
-    data = pipe.fit_transform(None)
+    df_preproc = pipe.fit_transform(df_raw)
+    logger.log(f'rows after preprocessing: {len(df_preproc)}')
 
     logger.log('saving data')
     curr_ts = datetime.now().strftime("%Y%m%d_%H%M%S")
     data_pickler = DataPickler(save_dir=curr_ts, file_name='df_prepro.pkl')
-    data_pickler.save(data)
+    data_pickler.save(df_preproc)
 
     logger.log('saving meta')
-    metadata = prep_df_metadata(X=data, pipe=pipe, git_parser=git_parser, ts=curr_ts)
+    metadata = prep_df_metadata(X=df_preproc, pipe=pipe, git_parser=git_parser, ts=curr_ts)
     metadata_pickler = DataPickler(save_dir=curr_ts, file_name='df_prepro_metadata.pkl')
     metadata_pickler.save(metadata)
 
-    logger.log(f'saved all to: {curr_ts} prepro_df shape: {data.shape}')
+    logger.log(f'saved all to: {curr_ts} df_preproc shape: {df_preproc.shape}')
--- src/pipelines.py
+++ src/pipelines.py
@@ -10,7 +10,6 @@ from src.preproc import RawDataCleaner, TextCleaner, SimpleTokenizer
 
 
 data_preproc_pipe = Pipeline([
-    ('load_data', CsvDataLoader(file_name='polish_sentiment_dataset.csv')),
     ('basic_cleaning', RawDataCleaner()),
     ('text_cleaning', TextCleaner()),
     ('tokenizer', SimpleTokenizer())
