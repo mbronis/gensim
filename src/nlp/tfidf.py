7cf196efae2708c1dc1ecc7591dfcd9fb5b9082f
mbronis <michal.bronis@gmail.com>
2021-10-28 20:18:52 +0200

    added loggging to tfidf transformer

--- src/nlp/tfidf.py
+++ src/nlp/tfidf.py
@@ -11,6 +11,10 @@ from gensim.models import TfidfModel
 
 from src.nlp.datatypes import Tokens
 from src.exceptions import TfIdfNotFitted
+from src.utils import Logger
+
+
+logger = Logger('tfidf')
 
 
 class TfIdfTransformer(BaseEstimator, TransformerMixin):
@@ -32,6 +36,7 @@ class TfIdfTransformer(BaseEstimator, TransformerMixin):
     def fit(self, docs: Iterable[Tokens], y: Iterable = None):
 
         # train dictionary
+        logger.log("fitting vocabulary")
         self.vocab = Dictionary(documents=docs)
 
         # filter vocab entries
@@ -43,8 +48,10 @@ class TfIdfTransformer(BaseEstimator, TransformerMixin):
         filter_extremes_params = {k: v for k, v in filter_extremes_params.items() if v is not None}
         if filter_extremes_params:
             self.vocab.filter_extremes(**filter_extremes_params)
+        logger.log(f"vocabulary fitted. unique tokens count: {len(self.vocab)}")
 
         # train tfifd model
+        logger.log("fitting tfidf model")
         model_params = {
             'normalize': self.normalize,
             'smartirs': self.smartirs,
@@ -52,18 +59,30 @@ class TfIdfTransformer(BaseEstimator, TransformerMixin):
         }
         model_params = {k: v for k, v in model_params.items() if v is not None}
         self.model = TfidfModel(dictionary=self.vocab, **model_params)
+        logger.log("tfidf model fitted")
 
         return self
 
     def transform(self, docs: Iterable[Tokens], y: Iterable = None):
         if not (self.vocab and self.model):
             raise TfIdfNotFitted()
+
+        logger.log("creating corpus")
         corpus = [self.vocab.doc2bow(doc) for doc in docs]
+        logger.log(f"corpus created. number of docs: {len(corpus)}")
+        logger.log("vectorizing corpus with tfidf model")
         tfidf_vectors = [self.model[bow] for bow in corpus]
 
         # sparse matrix reprezentation
+        logger.log("creating sparse corpus representation")
         i, j, data = zip(*((i, t[0], t[1]) for i, row in enumerate(tfidf_vectors) for t in row))
         tfidf_matrix = scipy.sparse.coo_matrix((data, (i, j)), shape=(len(corpus), len(self.vocab)))
 
+        logger.log("creating sparse df")
         columns = [self.vocab[i] for i in range(len(self.vocab))]
-        return pd.DataFrame.sparse.from_spmatrix(tfidf_matrix, columns=columns).sparse.to_dense()
+        df_sparse = pd.DataFrame.sparse.from_spmatrix(tfidf_matrix, columns=columns)
+
+        logger.log("creating dense df")
+        df_dense = df_sparse.sparse.to_dense()
+
+        return df_dense
